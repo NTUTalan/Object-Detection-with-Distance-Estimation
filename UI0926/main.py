3e42ec9bc3ab12d66785724b4eae4bb2d583cf52
ChunWang30832 <ironman30832@gmail.com>
2023-09-26 15:32:44 +0800

    0926影片截圖功能
    按下照相機按鈕會儲存一個整個視窗的圖片
    會存在一個檔名為當前時間的資料夾
    圖片檔名為當前執行的第n張

--- UI0926/main.py
+++ UI0926/main.py
@@ -6,20 +6,31 @@ from PyQt5 import uic
 from PyQt5.QtMultimedia import *
 from PyQt5.QtMultimediaWidgets import*
 
+import os
+import datetime
+
 import cv2
 import sys
 
 from UI import Ui_MainWindow
-
+#x=0
 
 class myMainWindow(Ui_MainWindow):
     def __init__(self, parent=None):
+        
         Ui_MainWindow.__init__(self)
         #Ui_MainWindow.__init__(self)
         #uic.loadUi("untitled.ui",self)
+        self.count=1
         self.setup()
         self.makeConnections()
         self.setWindowTitle("Video Player")
+
+        # 获取当前时间
+        current_time = datetime.datetime.now()
+        # 格式化时间为字符串，例如："2023-09-26_14-30-15"
+        self.formatted_time = current_time.strftime("%Y-%m-%d_%H-%M-%S")
+        
         
     def setup(self):
         self.videoOutput = self.makeVideoWidget()
@@ -42,8 +53,8 @@ class myMainWindow(Ui_MainWindow):
         self.button_play.clicked.connect(self.mediaPlayer.play)
         self.button_pause.clicked.connect(self.mediaPlayer.pause)
         self.button_stop.clicked.connect(self.mediaPlayer.stop)
-        self.button_shot.clicked.connect(self.capture_picture)
 
+        self.button_shot.clicked.connect(self.capture_picture)
         #影片進度條更新
         self.mediaPlayer.mediaStatusChanged.connect(self.onMediaStatusChanged)
         
@@ -72,23 +83,27 @@ class myMainWindow(Ui_MainWindow):
             self.slider_videoframe.sliderMoved.connect(self.setPosition)
             #print("影片總長度：", duration)
     def capture_picture(self):
-        video_frame = self.mediaPlayer.currentMedia()
-        print(video_frame)
-        #image = self.video_frame_to_image(video_frame)
-        #cv2.imwrite('captured_frame.png', image)
-        #print('Frame captured and saved as captured_frame.png')
-    # def video_frame_to_image(self, frame):
-    #     image = QImage(
-    #         frame.bits(),
-    #         frame.width(),
-    #         frame.height(),
-    #         frame.bytesPerLine(),
-    #         QImage.Format_RGB888
-    #     )
+        screen = QScreen.grabWindow(QApplication.primaryScreen(), self.winId())
+        image = QPixmap(screen)
 
-        # Convert QImage to OpenCV format
-        # cv_image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        # return cv_image
+        # 生成新的文件名，例如："2023-09-26_14-30-15.jpg"
+        new_filename = str(self.count)+".png"
+        self.count+=1
+
+        output_folder = self.formatted_time
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
+
+        # 生成新的文件名，例如："2023-09-26_14-30-15.jpg"，保存在"image"文件夹中
+        new_filename = os.path.join(output_folder, new_filename)
+
+        # 保存图像
+        image.save(new_filename)
+
+        print(f"图像已保存为: {new_filename}")
+
+        
+        
 
 
 # start.py
